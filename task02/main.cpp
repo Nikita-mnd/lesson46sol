52da153d08b5b959e63804e2e814387d1861db33
Nikita-mnd <nikita.morozov.mnd@gmail.com>
2025-03-24 16:18:05 +0300

    Add project files.

---
+++ task02/main.cpp
@@ -0,0 +1,51 @@
+#include "main.h"
+
+int main() {
+
+	int n, m;
+	int** matrix;
+
+	cout << "input size of the matrix(n and m): " << endl;
+	cin >> n >> m;
+	
+	matrix = new int*[n];
+	int size = n * m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			matrix[i][j] = rand() % 100;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+
+		for (int j = 0; j < m; j++) {
+
+			cout << matrix[m * i + j] << " ";
+
+		}
+		cout << endl;
+	}
+	//// logic
+	//int imax = 0;
+	//int jmax = 0;
+	//for (int i = 0; i < n; i++) {
+	//	for (int j = 0; j < m; j++) {
+	//		if (matrix[m * i + j] > matrix[m * imax + jmax]) {
+	//			imax = i;
+	//			jmax = j;
+	//		}
+	//	}
+	//}
+
+	
+//cout << "max element - matrix[" << imax + 1 << "][" << jmax + 1 << "]" << endl;
+
+	for (int i = 0; i < n; i++) {
+		delete[] matrix[i];
+
+	}
+
+	delete[] matrix;
+
+	return 0;
+}
